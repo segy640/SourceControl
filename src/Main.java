a031e82255813ffbab60c27714647d45fec942f2
Ivan <ivan.segrt.sambasador@gmail.com>
2022-11-22 23:13:29 +0100

    Add print statements. Hello GitHub

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@ public class Main {
     public static void main(String[] args) {
         //VCS  -> enable control integration -> GIT
         System.out.println("Hello world!");
+
+        int x = 1;
+        //Repository -> develop (Master)
+        //--feature
+        //--feature
+        System.out.println("Hello Github");
+        System.out.println("This is pushed to Github");
     }
 }
